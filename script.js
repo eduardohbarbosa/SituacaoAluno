89e3d0a6ad66deedbbf2c70e68f87cf6f851e627
EDUARDO HENRIQUE <eduhenrique5478@gmail.com>
2021-09-13 12:26:37 -0300

    criação dos arquivos

---
+++ script.js
@@ -0,0 +1,8 @@
+var n1 = document.querySelector('#n1').value
+var n2 = document.querySelector('#n2').value
+var n3 = document.querySelector('#n3').value
+var n4 = document.querySelector('#n4')
+
+function calcular(){
+    alert(n1, n2, n3, n4)
+}
